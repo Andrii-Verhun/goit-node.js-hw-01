bb79d2a1463f5a94e023793c8400bb08bda7eb7d
Andrii-Verhun <113340615+Andrii-Verhun@users.noreply.github.com>
2023-06-17 18:33:57 +0300

    add, invokeAction

--- contacts.js
+++ contacts.js
@@ -1,34 +1,45 @@
 const fs = require('fs/promises');
 const path = require('path');
+const { nanoid } = require('nanoid');
 
 const contactsPath = path.join(__dirname, 'db/contacts.json');
 
-async function listContacts() {
-    const contacts = await fs.readFile(contactsPath, 'utf-8');
-    return JSON.parse(contacts);
+async function listContacts(isLog = false) {
+  const contacts = await fs.readFile(contactsPath, 'utf-8');
+  if (isLog) console.log(JSON.parse(contacts));
+  return JSON.parse(contacts);
 };
 
 async function getContactById(contactId) {
-    const contacts = await listContacts();
-    const index = contacts.findIndex((item) => item.id === contactId);
-    console.log(contacts[index] || null);
-    return contacts[index] || null;
+  const contacts = await listContacts();
+  const index = contacts.findIndex((item) => item.id === contactId);
+  console.log(contacts[index] || null);
 };
 
 async function removeContact(contactId) {
-  // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
-    const contacts = await listContacts();
-    const index = contacts.findIndex((item) => item.id === contactId);
-    if (index === -1) return null;
+  const contacts = await listContacts();
+  const index = contacts.findIndex((item) => item.id === contactId);
+  if (index === -1) {
+    console.log(null);
+    return;
+    };
     const removeContact = contacts[index];
     contacts.splice(index, 1);
     await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
     console.log(removeContact);
-    return removeContact;
-}
+};
 
-function addContact(name, email, phone) {
-  // ...твій код. Повертає об'єкт доданого контакту. 
-}
+async function addContact(name, email, phone) {
+  const contacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    name,
+    email,
+    phone,
+  };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  console.log(newContact);
+};
 
-// removeContact('qdggE76Jtbfd9eWJHrssH11')
+module.exports = { listContacts, getContactById, removeContact, addContact };
